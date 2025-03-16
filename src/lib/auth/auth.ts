8b40f3a808a5caadc657d696bde293f2e9a6406e
Ishan130803 <ishan130803@gmail.com>
2025-03-16 19:06:35 +0530

    without auth configuration completed

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,10 +1,18 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
 import { AuthProviders } from "./providers";
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "../prisma";
+// import { PrismaAdapter } from "@auth/prisma-adapter"
+// import { prisma } from "../prisma";
 
 export const authOptions: NextAuthConfig = {
   providers: AuthProviders,
-  adapter: PrismaAdapter(prisma),
+  // adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session({session, token}) {
+      if (session) {
+        session.userId = token?.sub as string;
+      }
+      return session;
+    }
+  }
 };
 export const { handlers, signIn, signOut, auth } = NextAuth(authOptions);
