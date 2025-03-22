e917059f3772ebed130354e75b57b06787b30412
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2025-03-22 10:09:21 +0530

    add basic sidebar

--- src/app/(logged-in)/layout.tsx
+++ src/app/(logged-in)/layout.tsx
@@ -1,14 +1,14 @@
 "use server";
 import { Navbar } from "@/components/navbar";
 import EnsureLoggedIn from "@/features/auth/components/ensure-logged-in";
-// Colors are temporary to see if everything fits in intended layout or not
+
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="w-full max-w-[2160px] min-h-screen bg-orange-100 overflow-hidden">
+    <div className="w-full relative max-w-[2160px] min-h-screen overflow-hidden">
       <Navbar />
       <main className="w-full">
         <EnsureLoggedIn>{children}</EnsureLoggedIn>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,49 @@
+"use client";
+import { useSidebar } from "@/hooks/use-sidebar";
+import { cn } from "@/lib/utils";
+import { DraftingCompass, FileCheck2 } from "lucide-react";
+import { ResponsiveSidebar } from "./responsive-sidebar";
 import { UserAvatar } from "./user-avatar";
 
 function Navbar() {
+  const { sidebarState } = useSidebar({ name: "app-sidebar" });
   return (
-    <nav className="px-6 py-2 flex items-center bg-red-100">
-      <div className="flex-grow"></div>
-      <UserAvatar className="m-4" />
+    <nav>
+      <ResponsiveSidebar>
+        {/* Menu */}
+        <div className="flex sm:flex-col gap-2 sm:gap-5">
+          <button className="flex gap-2 items-center p-3 rounded-xl bg-zinc-400 text-zinc-900 mx-2">
+            <FileCheck2
+              className={cn("size-4", {
+                "md:size-5": sidebarState === "collapsed",
+              })}
+            />
+            {sidebarState === "collapsed" ? (
+              <span className="sm:hidden text-xs md:text-sm lg:text-base">
+                Docs
+              </span>
+            ) : (
+              <span className="text-xs md:text-sm lg:text-base">Docs</span>
+            )}
+          </button>
+          <button className="flex gap-2 items-center p-3 rounded-xl mx-2 bg-zinc-800">
+            <DraftingCompass
+              className={cn("size-4", {
+                "md:size-5": sidebarState === "collapsed",
+              })}
+            />
+            {sidebarState === "collapsed" ? (
+              <span className="sm:hidden text-xs md:text-sm lg:text-base">
+                Drafts
+              </span>
+            ) : (
+              <span className="text-xs md:text-sm lg:text-base">Drafts</span>
+            )}
+          </button>
+        </div>
+        {/* User */}
+        <UserAvatar className="m-4 border-none ring-2 ring-zinc-700" />
+      </ResponsiveSidebar>
     </nav>
   );
 }
